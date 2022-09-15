1187b71fe3548ca88a18fc0dcf4309faafa694cc
1608579065 <113379326+1608579065@users.noreply.github.com>
2022-09-16 00:21:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,6 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "我的大宝贝", "year": "2001", "date": "10-10"},
         {"type": "节日", "name": "在一起纪念日", "year": "2022", "date": "03-26"},
                  ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
